391ddc2e37696d9062ea2c7919a636b0bbce3ce0
veropra <prainoveronica@gmail.com>
2019-05-13 01:25:20 +0200

    Es.terminato base+avanzato!

--- script.js
+++ script.js
@@ -6,13 +6,24 @@
 
   var next_img = img_corrente.next("img");
 
+  var bullet_corrente = $(".fas.fa-circle");
+
+  var next_bullet = bullet_corrente.next(".fa-circle");
+
   if(next_img.length != 0){
   next_img.addClass("visible");
+  bullet_corrente.toggleClass('fas').toggleClass('far');
+  next_bullet.toggleClass('fas').toggleClass('far');
+
   }else{
+
   $(".slider img").first().addClass("visible");
+  bullet_corrente.removeClass('fas').addClass('far');
+  next_bullet = $('.bullet i').first().addClass('fas').removeClass('far');
   }
 
 });
+
 
 $(".prev").click(function(){
 
@@ -20,13 +31,21 @@ $(".prev").click(function(){
 
   var precedente_img = img_corrente.prev("img");
 
+  var bullet_precedente = $('.fas.fa-circle');
+
+  var prev_bullet = bullet_precedente.prev('.fa-circle');
+
   if(precedente_img.length != 0){
   precedente_img.toggleClass("visible");
-  }else{
-  $(".slider img").last().addClass("visible");
-  }
+  bullet_precedente.toggleClass('fas').toggleClass('far');
+  prev_bullet.toggleClass('fas').toggleClass('far');
 
-  img_corrente.toggleClass("visible");
-  
+  }else{
+
+  $(".slider img").last().addClass("visible");
+  img_corrente.removeClass("visible");
+  bullet_precedente.removeClass('fas').addClass('far');
+  prev_bullet = $('.bullet i').last().addClass('fas').removeClass('far');
+  }
 
 });
